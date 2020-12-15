70deab5950d866bef1a8834fd2dc4d2ba77e24b5
du3ling <785045798@qq.com>
2020-12-15 12:18:45 +0800

    第四次提交： 增加了testdata_utils.py 实现excel数据转换成真正需要的接口业务数据的方法
    增加了samples下demo.py，测试数据转换的示例在里面

---
+++ common/testdata_utils.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/14 17:06
+# @Author : 杜云慧
+# @Site : 
+# @File : testdata_utils.py
+# @Software: PyCharm
+'''处理表格数据'''
+
+import os
+from common.excel_utils import ExcelUtils
+
+current_path = os.path.dirname(__file__)
+test_data_path = os.path.join(current_path, '..', 'test_data/test_case.xlsx')
+
+
+class TestdataUtils():
+    def __init__(self, test_data_path=test_data_path):
+        self.test_data_path = test_data_path
+        self.test_data = ExcelUtils(test_data_path, 'Sheet1').get_sheet_data_by_dict()
+
+    def __get_testcase_data_dict(self):
+        '''通过字典方式获取数据'''
+        testcase_dict = {}
+        for row_data in self.test_data:
+            testcase_dict.setdefault(row_data['测试用例编号'], []).append(row_data)
+        return testcase_dict
+
+    def def_testcase_data_list(self):
+        testcase_list = []
+        for k, v in self.__get_testcase_data_dict().items():
+            one_case_dict = {}
+            one_case_dict["case_name"] = k
+            one_case_dict["case_info"] = v
+            testcase_list.append(one_case_dict)
+        return testcase_list
+
+
+if __name__ == "__main__":
+    testdataUtils = TestdataUtils()
+    for i in testdataUtils.def_testcase_data_list():
+        print(i)
---
+++ samples/demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/14 16:52
+# @Author : 杜云慧
+# @Site : 
+# @File : demo.py
+# @Software: PyCharm
+
+a = {'one': 1, 'two': 2, 'three': 3}
+a.setdefault('four', 4)
+print(a)
